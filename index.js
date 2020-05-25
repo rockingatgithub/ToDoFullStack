29cf0e4d7686333c7eb4b67dc40690206cbc95ee
rockingatgithub <contactwithsid@gmail.com>
2020-05-26 01:03:07 +0530

    added tasklist page

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('assets'));
 
 app.get('/', function(req, res){
     return res.render('home',{
@@ -21,8 +23,21 @@ app.get('/practice', function(req, res){
     })
 })
 
+app.post('/add-task', function(req, res){
+    // console.log(req.body);
+    return res.redirect('/practice');
+})
 
+app.get('/task_list_page', function(req, res){
+    return res.render('task_list', {
+        title: 'your_tasks',
+        
+    });
+})
 
+app.get('/delete_multiple_tasks', function(req, res){
+    
+})
 
 app.listen(port, function(err){
    if(err){ console.log("error in runnng the server");}
